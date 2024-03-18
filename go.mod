6340f678974c53f82c4ab396892cff0e158a8e73
EzequielBPullolil <programacionezequielk@gmail.com>
2024-03-18 08:17:55 -0300

    feat: Added base layout and index.html template

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/EzequielBPullolil/cipher_view
 go 1.22.0
 
 require (
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/kataras/blocks v0.0.8 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
--- src/main.go
+++ src/main.go
@@ -4,11 +4,22 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi/v5"
+	"github.com/kataras/blocks"
 )
 
 func main() {
-	r := mux.NewRouter()
+	r := chi.NewRouter()
+	views := blocks.New("./src/views")
+	if err := views.Load(); err != nil {
+		log.Fatal(err)
+	}
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := views.ExecuteTemplate(w, "index", "base", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
+	})
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
